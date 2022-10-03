cca6d955a1342102448643b8f4ad8357b7f82aa8
Aleksandr Bashkov <abashkov@mail.ru>
2022-10-03 12:50:33 +0300

    В парсинге команды в split использовал регулярку вместо указания пробела. Рефакторинг для !anyMatch

--- src/main/java/ru/liga/homework1/CommandParser.java
+++ src/main/java/ru/liga/homework1/CommandParser.java
@@ -16,13 +16,13 @@ public class CommandParser {
     private final Period period;
 
     public CommandParser(String command) {
-        var cmdEntities = command.split(" ");
+        var cmdEntities = command.split("\\s+");
         if (cmdEntities == null || cmdEntities.length != 3 || !cmdEntities[0].equals("rate"))
             throw new InvalidParameterException("Invalid command");
-        if (!Arrays.stream(Currency.values()).anyMatch(cur -> cur.name().equals(cmdEntities[1])))
+        if (Arrays.stream(Currency.values()).noneMatch(cur -> cur.name().equals(cmdEntities[1])))
             throw new InvalidParameterException("Unknown currency " + cmdEntities[1]);
         currency = Currency.valueOf(cmdEntities[1]);
-        if (!Arrays.stream(Period.values()).anyMatch(per -> per.name().toLowerCase().equals(cmdEntities[2])))
+        if (Arrays.stream(Period.values()).noneMatch(per -> per.name().toLowerCase().equals(cmdEntities[2])))
             throw new InvalidParameterException("Unknown period " + cmdEntities[2]);
         period = Period.valueOf(cmdEntities[2].toUpperCase());
     }
