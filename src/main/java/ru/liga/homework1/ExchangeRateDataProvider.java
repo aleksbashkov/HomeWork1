18f77ec9211dcc2be1d16baf1c060303ce2c4baf
Aleksandr Bashkov <abashkov@mail.ru>
2022-10-10 18:59:53 +0300

    Дополняю набор курсов данными за отсутствующие дни.

--- src/main/java/ru/liga/homework1/ExchangeRateDataProvider.java
+++ src/main/java/ru/liga/homework1/ExchangeRateDataProvider.java
@@ -2,6 +2,8 @@ package ru.liga.homework1;
 
 import ru.liga.homework1.enums.Currency;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
 import java.util.List;
 
@@ -12,13 +14,29 @@ public class ExchangeRateDataProvider {
      * @return упорядоченный по дате список курсов валюты с датами
      */
     public static List<RateForDate> getSortedExchangeRateData(Currency currency) {
+        // считываем данные из файла:
         var reader = new ExchangeRateDataReader();
         var exchangeRateData = reader.readExchangeData(String.format("/%s.csv", currency.name()));
         if (exchangeRateData.size() == 0)
             throw new RuntimeException("Нет данных для валюты " + currency.name());
 
-        return exchangeRateData.stream()
-                .sorted(Comparator.comparing(RateForDate::getDate))
-                .toList();
+        // сортируем по дате:
+        exchangeRateData.sort(Comparator.comparing(RateForDate::getDate));
+
+        // дополняем значениями для отсутствующих дней:
+        addMissingExchangeRate(exchangeRateData);
+
+        return exchangeRateData;
+    }
+
+    private static void addMissingExchangeRate(List<RateForDate> exchangeRateData) {
+        var firstDate = exchangeRateData.get(0).getDate();
+        var lastDate = exchangeRateData.get(exchangeRateData.size()-1).getDate();
+        var dayCnt = ChronoUnit.DAYS.between(firstDate, lastDate);
+        for (int i = 1; i < dayCnt; i++) {
+            LocalDate curDate = firstDate.plusDays(i);
+            if (!exchangeRateData.get(i).getDate().equals(curDate)) // отсутствует курс за дату
+                exchangeRateData.add(i, new RateForDate(curDate, exchangeRateData.get(i-1).getRate())); // берём предыдущее значение курса
+        }
     }
 }
