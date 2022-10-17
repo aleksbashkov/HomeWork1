eddf97adcbf3796e6bcc212a0aec374f93033f36
Aleksandr Bashkov <abashkov@mail.ru>
2022-10-17 14:35:47 +0300

    Дополнил тесты для класса ExchangeRateDataReader.

--- src/test/java/ru/liga/homework1/ExchangeRateDataReaderTest.java
+++ src/test/java/ru/liga/homework1/ExchangeRateDataReaderTest.java
@@ -14,6 +14,13 @@ class ExchangeRateDataReaderTest {
         assertThrows(RuntimeException.class, () -> reader.readExchangeData("SomeNonexistentFile.csv"));
     }
 
+    @DisplayName("Кидает исключение, когда на вход подаём файл с некорректным содержанием")
+    @Test
+    void shouldThrowNumberFormatException() {
+        var reader = new ExchangeRateDataReader();
+        assertThrows(NumberFormatException.class, () -> reader.readExchangeData("EUR_wrong_data.csv"));
+    }
+
     @DisplayName("Проверяем, что в заданном файле 5 записей")
     @Test
     void checkFiveRecordsInFile() {
@@ -22,3 +29,33 @@ class ExchangeRateDataReaderTest {
         assertEquals(5, data.size());
     }
 }
+
+
+//    /**
+//     * Аналитик попросил посмотреть есть ли и какие города с именами больше 16 символов (чтобы влезло в GUI на вебе)
+//     * <p>
+//     * Вывести все города, имена которых больше 16 символов, игнорировать города с пробелами в названии, отсортировать по названию
+//     */
+//    @Test
+//    public void test() {
+//        File file = new File(
+//                getClass().getClassLoader().getResource("world.sqlite").getFile()
+//        );
+
+//
+//        var reporter = new Reporter(new ReportBuilder(), new EmailReportSender());
+//        Assertions.assertThrows(NoReportsException.class, () -> reporter.sendReports());
+//
+//        assertThat(world.getCountries().get(0).hasCitiesWithThreeOrLessLetterName()).isEqualTo(true);
+//        assertThat(world.getCountries().get(2).hasCitiesWithThreeOrLessLetterName()).isEqualTo(false);
+//
+//        World world = new WorldRepository(file.getAbsolutePath()).helloWorld();
+//        assertThat(world.threeOrLessLetterNameOfCityCountryCount()).isEqualTo(54);
+
+
+//        World world = new WorldRepository(file.getAbsolutePath()).helloWorld();
+//        List<City> cityList = world.getAllCitiesNamesWithoutWhitespacesMoreSixteenSymbolsSortedByName();
+//        System.out.println(cityList);
+//        assertThat(cityList.size()).isEqualTo(188);
+//        assertThat(cityList.get(1).getName()).isEqualTo("Abergement-le-Petit");
+//    }
