b42f11bf96bb10429ec6a8fb735a98a0febea788
Aleksandr Bashkov <abashkov@mail.ru>
2022-10-17 23:40:10 +0300

    Расширил класс ExchangeRateDataProvider.

--- src/main/java/ru/liga/homework1/ExchangeRateDataProvider.java
+++ src/main/java/ru/liga/homework1/ExchangeRateDataProvider.java
@@ -9,17 +9,18 @@ import java.util.Comparator;
 import java.util.List;
 
 public class ExchangeRateDataProvider {
+
     /**
-     * Предоставляет курсы для конкретной валюты
-     * @param currency - валюта
+     * Предоставляет курсы из файла
+     * @param fileName - имя файла с курсами валюты
      * @return упорядоченный по дате список курсов валюты с датами
      */
-    public static List<RateForDate> getSortedExchangeRateData(Currency currency) {
+    public static List<RateForDate> getSortedExchangeRateData(String fileName) {
         // считываем данные из файла:
         var reader = new ExchangeRateDataReader();
-        var exchangeRateData = reader.readExchangeData(String.format("%s.csv", currency.name()));
+        var exchangeRateData = reader.readExchangeData(fileName);
         if (exchangeRateData.size() == 0)
-            throw new RuntimeException("Нет данных для валюты " + currency.name());
+            throw new RuntimeException("Нет данных в файле " + fileName);
 
         // сортируем по дате:
         exchangeRateData.sort(Comparator.comparing(RateForDate::getDate));
@@ -28,6 +29,15 @@ public class ExchangeRateDataProvider {
         addMissingExchangeRate(exchangeRateData);
 
         return exchangeRateData;
+    }
+
+    /**
+     * Предоставляет курсы для конкретной валюты
+     * @param currency - валюта
+     * @return упорядоченный по дате список курсов валюты с датами
+     */
+    public static List<RateForDate> getSortedExchangeRateData(Currency currency) {
+        return getSortedExchangeRateData(String.format("%s.csv", currency.name()));
     }
 
     private static void addMissingExchangeRate(List<RateForDate> exchangeRateData) {
