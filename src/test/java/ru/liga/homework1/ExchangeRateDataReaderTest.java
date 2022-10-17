9170c62f592ee327d90a90e4e9b05feeef4be538
Aleksandr Bashkov <abashkov@mail.ru>
2022-10-17 23:44:20 +0300

    Выкинул лишнее.

--- src/test/java/ru/liga/homework1/ExchangeRateDataReaderTest.java
+++ src/test/java/ru/liga/homework1/ExchangeRateDataReaderTest.java
@@ -29,33 +29,3 @@ class ExchangeRateDataReaderTest {
         assertEquals(5, data.size());
     }
 }
-
-
-//    /**
-//     * Аналитик попросил посмотреть есть ли и какие города с именами больше 16 символов (чтобы влезло в GUI на вебе)
-//     * <p>
-//     * Вывести все города, имена которых больше 16 символов, игнорировать города с пробелами в названии, отсортировать по названию
-//     */
-//    @Test
-//    public void test() {
-//        File file = new File(
-//                getClass().getClassLoader().getResource("world.sqlite").getFile()
-//        );
-
-//
-//        var reporter = new Reporter(new ReportBuilder(), new EmailReportSender());
-//        Assertions.assertThrows(NoReportsException.class, () -> reporter.sendReports());
-//
-//        assertThat(world.getCountries().get(0).hasCitiesWithThreeOrLessLetterName()).isEqualTo(true);
-//        assertThat(world.getCountries().get(2).hasCitiesWithThreeOrLessLetterName()).isEqualTo(false);
-//
-//        World world = new WorldRepository(file.getAbsolutePath()).helloWorld();
-//        assertThat(world.threeOrLessLetterNameOfCityCountryCount()).isEqualTo(54);
-
-
-//        World world = new WorldRepository(file.getAbsolutePath()).helloWorld();
-//        List<City> cityList = world.getAllCitiesNamesWithoutWhitespacesMoreSixteenSymbolsSortedByName();
-//        System.out.println(cityList);
-//        assertThat(cityList.size()).isEqualTo(188);
-//        assertThat(cityList.get(1).getName()).isEqualTo("Abergement-le-Petit");
-//    }
