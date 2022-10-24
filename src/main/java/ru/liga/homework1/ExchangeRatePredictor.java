f8f540b21397ef9f4f0054e519305dfdde85cf38
Aleksandr Bashkov <abashkov@mail.ru>
2022-10-25 00:37:18 +0300

    Некоторые переделки в классах Main и ExchangeRatePredictor.

--- src/main/java/ru/liga/homework1/ExchangeRatePredictor.java
+++ src/main/java/ru/liga/homework1/ExchangeRatePredictor.java
@@ -1,14 +1,15 @@
 package ru.liga.homework1;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import ru.liga.homework1.algorithms.PredictionAlgorithm;
-import ru.liga.homework1.enums.Period;
 import ru.liga.homework1.model.RateForDate;
 
+/**
+ * Класс осуществляет прогнозирование курсов в соответствии с переданным ему в конструктор алгоритмом
+ */
 public class ExchangeRatePredictor {
 
     final PredictionAlgorithm algorithm;
@@ -17,37 +18,16 @@ public class ExchangeRatePredictor {
         this.algorithm = algorithm;
     }
 
-//    /**
-//     * Осуществляет прогнозирование курса валюты
-//      * @param algorithm алгоритм, который осуществляет прогнозирование
-//     * @param period - период, на который осуществляется прогнозирование
-//     * @return - прогноз в виде списка объектов типа RateForDate
-//     */
-    public List<RateForDate> doPrediction(List<RateForDate> incomingData, Period period) {
+    /**
+     * Прогнозирование курсов валют по исходным данным на переданные даты
+     * @param incomingData - входные данные с курсами
+     * @param dates - список дат, на которые делается прогноз
+     * @return прогноз в виде списка объектов типа RateForDate
+     */
+    public List<RateForDate> doPrediction(List<RateForDate> incomingData, List<LocalDate> dates) {
         var result = new ArrayList<RateForDate>();
-
-        var tomorrow = LocalDate.now().plusDays(1);
-        var last = incomingData.get(incomingData.size()-1);
-        BigDecimal tomorrowValue;
-        if (last.getDate().equals(tomorrow)) { // завтрашний курс уже может быть во входящих данных, тогда его прогнозировать не надо
-            tomorrowValue = last.getRate();
-        }
-        else {
-            tomorrowValue = algorithm.doPrediction(incomingData, tomorrow);
-            incomingData.add(new RateForDate(tomorrow, tomorrowValue));
-        }
-        result.add(new RateForDate(tomorrow, tomorrowValue)); // добавляем в результат значение на завтра
-
-        if (period == Period.WEEK) { // если нужен прогноз на неделю, то делаем прогнозирование ещё на 6 дней
-            for (int i = 1; i < 7; i++) {
-                var newDate = tomorrow.plusDays(i);
-                var newValue = algorithm.doPrediction(incomingData, newDate);
-                var newRate = new RateForDate(newDate, newValue);
-                result.add(newRate);
-                incomingData.add(newRate);
-            }
-        }
-
+        for (var date : dates)
+            result.add(new RateForDate(date, algorithm.doPrediction(incomingData, date)));
         return result;
     }
 }
--- src/main/java/ru/liga/homework1/Main.java
+++ src/main/java/ru/liga/homework1/Main.java
@@ -1,11 +1,14 @@
 package ru.liga.homework1;
 
 import ru.liga.homework1.algorithms.AlgorithmFactory;
+import ru.liga.homework1.enums.Period;
 import ru.liga.homework1.exceptions.InvalidCommandException;
 import ru.liga.homework1.exceptions.InvalidOutputParametersException;
+import ru.liga.homework1.model.CommandParameters;
 import ru.liga.homework1.model.CurrencyData;
 import ru.liga.homework1.outputs.OutputFactory;
 
+import java.time.LocalDate;
 import java.util.*;
 
 public class Main {
@@ -38,16 +41,34 @@ public class Main {
             var exchangeRateData = ExchangeRateDataProvider.getSortedExchangeRateData(currency);
 
             // прогнозируем курсы:
-            var result = predictor.doPrediction(exchangeRateData, parameters.getPeriod());//?? getDate?
+            var result = predictor.doPrediction(exchangeRateData, getDates(parameters));
             predictionResult.add(new CurrencyData(currency, result));
         }
 
+        // выводим результат:
         var output = OutputFactory.getOutput(parameters.getOutputType());
         try {
             output.doOutput(predictionResult);
         } catch (InvalidOutputParametersException e) {
             //??
         }
+    }
 
+    private static List<LocalDate> getDates(CommandParameters parameters) {
+        var result = new ArrayList<LocalDate>();
+        var date = parameters.getDate();
+        if (date != null)
+            result.add(date);
+        else {
+            LocalDate curDate = LocalDate.now().plusDays(1);
+            LocalDate finishDate = (parameters.getPeriod() == Period.WEEK)
+                ? curDate.plusWeeks(1)
+                : curDate.plusMonths(1);
+            do {
+                result.add(curDate);
+                curDate = curDate.plusDays(1);
+            } while (!curDate.isAfter(finishDate));
+        }
+        return result;
     }
 }
